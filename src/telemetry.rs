28dffbb5dda5cd24217c842f3e5d7e4f4237a2dd
fomotoshi <fomotoshi@ya.ru>
2022-01-31 10:17:58 +0800

    minor fixes

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,4 +1,4 @@
-use tracing::{subscriber::set_global_default, Subscriber};
+use tracing::{subscriber, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
@@ -15,5 +15,5 @@ pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Sen
 
 pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
     LogTracer::init().expect("Failed to set logger");
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
 }
