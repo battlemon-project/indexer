c507321c48af43fe50c1d903f25862c80d8e3cce
fomotoshi <fomotoshi@ya.ru>
2022-02-02 09:30:16 +0800

    - updated model `Sale`

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,15 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
 pub enum Events {
-    Sale
+    Sale(Sale),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Sale {
+    pub prev: String,
+    pub curr: String,
+    pub token_id: String,
+    pub price: String,
 }
