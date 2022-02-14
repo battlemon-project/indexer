d4e5762dde4824a69d23c5a425303aa571fb4f3c
fomotoshi <fomotoshi@ya.ru>
2022-02-14 12:42:28 +0800

    - refactored consts.rs

--- src/consts.rs
+++ src/consts.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use near_indexer::near_primitives::types::AccountId;
 use tokio::sync::OnceCell;
 
@@ -5,7 +7,11 @@ static MAIN_ACC: OnceCell<AccountId> = OnceCell::const_new();
 
 pub async fn get_main_acc() -> &'static AccountId {
     MAIN_ACC
-        .get_or_init(|| async { "test.near".parse().expect("Couldn't parse provided name") })
+        .get_or_init(|| async {
+            let config = crate::config::get_config::<crate::config::RunSettings>()
+                .expect("Couldn't run indexer");
+            AccountId::from_str(&config.main_account).expect("Account from config isn't valid")
+        })
         .await
 }
 
