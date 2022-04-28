800c30dba18628901bad9832ed46e32c062b4942
ChzenChzen <fedorovdanila@me.com>
2022-04-28 11:34:20 +0800

    update deps and modify migrations

--- migrations/20220127230432_create_sales_table.sql
+++ migrations/20220127230432_create_sales_table.sql
@@ -6,6 +6,6 @@ CREATE TABLE sales
     prev_owner TEXT        NOT NULL,
     curr_owner TEXT        NOT NULL,
     token_id   TEXT        NOT NULL,
-    price      TEXT        NOT NULL,
+    price      NUMERIC     NOT NULL,
     date       timestamptz NOT NULL
 );
