1864df597e541ee58484095667eb630a6ae994b4
ChzenChzen <fedorovdanila@me.com>
2022-05-01 20:19:54 +0800

    implement settigs for aws

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,33 @@
+use near_lake_framework::LakeConfig;
 use serde::Deserialize;
 use sqlx::postgres::PgConnectOptions;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
-pub struct RunSettings {
+pub struct AppSettings {
     pub contract_acc: String,
-    pub block: u64,
     pub database: DatabaseSettings,
+    pub aws: AwsSettings,
+}
+
+#[derive(Deserialize)]
+pub struct AwsSettings {
+    pub s3_endpoint: Option<String>,
+    pub s3_bucket_name: String,
+    pub s3_region_name: String,
+    pub start_block_height: u64,
+}
+
+
+impl From<AwsSettings> for LakeConfig {
+    fn from(aws: AwsSettings) -> Self {
+        Self {
+            s3_endpoint: aws.s3_endpoint,
+            s3_bucket_name: aws.s3_bucket_name,
+            s3_region_name: aws.s3_region_name,
+            start_block_height: aws.start_block_height,
+        }
+    }
 }
 
 #[derive(Deserialize)]
@@ -36,10 +57,7 @@ impl DatabaseSettings {
     name = "Loading configuration from file `config.yaml`",
     fields(id = %Uuid::new_v4())
 )]
-pub fn get_config<'de, T>() -> Result<T, config::ConfigError>
-where
-    T: Deserialize<'de>,
-{
+pub fn get_config() -> Result<AppSettings, config::ConfigError> {
     let mut settings = config::Config::default();
     settings.merge(config::File::with_name("config"))?;
     settings.try_into()
