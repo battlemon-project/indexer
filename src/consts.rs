217421f21e1b193800367861060a557726994cb6
ChzenChzen <fedorovdanila@me.com>
2022-05-02 12:28:18 +0800

    fix imports and error message

--- src/consts.rs
+++ src/consts.rs
@@ -1,11 +1,11 @@
 use crate::config::AppSettings;
-use near_lake_framework::near_indexer_primitives::types::AccountId;
-use std::str::FromStr;
 use tokio::sync::OnceCell;
 
 static CONFIG: OnceCell<AppSettings> = OnceCell::const_new();
 pub async fn get_config() -> &'static AppSettings {
     CONFIG
-        .get_or_init(|| async { crate::config::load_config().expect("Couldn't run indexer") })
+        .get_or_init(|| async {
+            crate::config::load_config().expect("Couldn't load config for indexer")
+        })
         .await
 }
