36e23cd0b5ecd693af356901caa740ef9dc07d29
ChzenChzen <fedorovdanila@me.com>
2022-05-02 11:45:10 +0800

    refactor main with new config loading

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,29 @@
-use near_jsonrpc_client::{JsonRpcClient,  NEAR_TESTNET_ARCHIVAL_RPC_URL};
-use near_lake_framework::LakeConfig;
+use battlemon_near_json_rpc_client_wrapper::{JsonRpcWrapper, NEAR_TESTNET_ARCHIVAL_RPC_URL};
 use sqlx::postgres::PgPoolOptions;
 
-use battlemon_indexer::config::{get_config, RunSettings};
+use battlemon_indexer::consts::get_config;
 use battlemon_indexer::{startup, telemetry, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let subscriber = telemetry::get_subscriber("battlemon_indexer".into(), "info".into());
     telemetry::init_subscriber(subscriber);
-    let config = get_config::<RunSettings>().expect("Couldn't run indexer");
+    let config = get_config().await;
     tracing::info!("Config was loaded");
-    tracing::info!("NFT Contract account is {}", config.contract_acc);
+    tracing::info!(
+        "Root contract account is {}",
+        config.contracts.top_contract_id
+    );
     tracing::info!("Get connection config for database");
     let pool_conn = PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(config.database.with_db());
-    let stream = near_lake_framework::streamer(LakeConfig {
-        s3_endpoint: None,
-        s3_bucket_name: "near-lake-data-testnet".to_string(),
-        s3_region_name: "eu-central-1".to_string(),
-        start_block_height: config.block,
-    });
+    let stream = near_lake_framework::streamer(config.aws.clone().into());
     // todo: add to config testnet or mainnet setting for rpc
-    let rpc_client = JsonRpcClient::connect(NEAR_TESTNET_ARCHIVAL_RPC_URL);
+    let rpc_client = JsonRpcWrapper::connect(
+        NEAR_TESTNET_ARCHIVAL_RPC_URL,
+        config.near_credentials.clone().into(),
+    );
 
     startup::run_indexer(stream, pool_conn, rpc_client)
         .await
