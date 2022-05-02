9ad7dc14fcbf57e00a3de555a9ee692f84f060c3
ChzenChzen <fedorovdanila@me.com>
2022-05-02 11:23:52 +0800

    - make lazy init of config - delete redundant

--- src/consts.rs
+++ src/consts.rs
@@ -1,15 +1,11 @@
-use std::str::FromStr;
+use crate::config::AppSettings;
 use near_lake_framework::near_indexer_primitives::types::AccountId;
+use std::str::FromStr;
 use tokio::sync::OnceCell;
 
-static MAIN_ACC: OnceCell<AccountId> = OnceCell::const_new();
-
-pub async fn get_contract_acc() -> &'static AccountId {
-    MAIN_ACC
-        .get_or_init(|| async {
-            let config = crate::config::get_config::<crate::config::RunSettings>()
-               .expect("Couldn't run indexer");
-            AccountId::from_str(&config.contract_acc).expect("Account from config isn't valid")
-        })
+static CONFIG: OnceCell<AppSettings> = OnceCell::const_new();
+pub async fn get_config() -> &'static AppSettings {
+    CONFIG
+        .get_or_init(|| async { crate::config::load_config().expect("Couldn't run indexer") })
         .await
 }
