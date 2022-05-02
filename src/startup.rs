6596b657ba9088b97f462e67910c1367f5cd36bb
ChzenChzen <fedorovdanila@me.com>
2022-05-02 11:50:25 +0800

    add rpc wrapper to startup.rs

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,14 @@
 use actix_web::web;
-use near_jsonrpc_client::JsonRpcClient;
+use battlemon_near_json_rpc_client_wrapper::JsonRpcWrapper;
 use tokio::sync::mpsc;
 
 use crate::{handle_message, PgPool, StreamerMessage};
 
-#[tracing::instrument(name = "Run indexer", skip(stream, pool_conn))]
+#[tracing::instrument(name = "Run indexer", skip(stream, pool_conn, rpc_client))]
 pub async fn run_indexer(
     mut stream: mpsc::Receiver<StreamerMessage>,
     pool_conn: PgPool,
-    rpc_client: JsonRpcClient,
+    rpc_client: JsonRpcWrapper,
 ) -> crate::Result<()> {
     let pool_conn = web::Data::new(pool_conn);
     let rpc_client = web::Data::new(rpc_client);
