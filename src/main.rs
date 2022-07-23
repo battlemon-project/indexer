040f1f8ec814e8ebb6bcc915388178f66a12db61
ChzenChzen <fedorovdanila@me.com>
2022-07-23 14:35:24 +0300

    refactor initialization of service

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,18 @@
-use battlemon_near_json_rpc_client_wrapper::{JsonRpcWrapper, NEAR_TESTNET_ARCHIVAL_RPC_URL};
-use sqlx::postgres::PgPoolOptions;
-
-use battlemon_indexer::consts::get_config;
-use battlemon_indexer::{startup, telemetry, Result};
+use battlemon_indexer::config::get_config;
+use battlemon_indexer::{startup, telemetry};
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> anyhow::Result<()> {
     let subscriber = telemetry::get_subscriber("battlemon_indexer".into(), "info".into());
     telemetry::init_subscriber(subscriber);
     let config = get_config().await;
-    tracing::info!("Config was loaded");
-    tracing::info!(
-        "Root contract account is {}",
-        config.contracts.top_contract_id
-    );
-    tracing::info!("Get connection config for database");
-    let pool_conn = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(config.database.with_db());
-    let rpc_client = JsonRpcWrapper::connect(
-        NEAR_TESTNET_ARCHIVAL_RPC_URL,
-        config.near_credentials.clone().into(),
-    );
-    tracing::info!("Loading config for NEAR Lake Framework");
-    let lake_config = config.aws.lake_config(&rpc_client).await?;
-    let stream = near_lake_framework::streamer(lake_config);
-    // todo: add to config testnet or mainnet setting for rpc
+    tracing::info!("Loading configuration for NEAR Lake Framework");
+    let lake_config = config.near_lake.near_lake_config().await?;
+    let client = reqwest::Client::new();
+    tracing::info!("Starting up NEAR Lake Framework");
+    let stream = near_lake_framework::streamer(lake_config).1;
 
-    startup::run_indexer(stream, pool_conn, rpc_client)
+    startup::run_indexer(stream, client)
         .await
         .expect("Couldn't run indexer");
     Ok(())
