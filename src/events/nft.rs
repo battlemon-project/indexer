7897caf83963e6ec6419d9e0938df7a64479dc4c
ChzenChzen <fedorovdanila@me.com>
2022-10-18 21:15:07 +0300

    refactor deserialize outcome

--- src/events/nft.rs
+++ src/events/nft.rs
@@ -2,23 +2,31 @@ use crate::{events, get_config, ExecutionStatusView, IndexerExecutionOutcomeWith
 use actix_web::web;
 use anyhow::{anyhow, Context};
 use battlemon_models::nft::{NftEvent, NftEventKind, NftTokenForRest, TokenExt};
+use serde::{de::DeserializeOwned, Deserialize};
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+enum OneOrMany<T> {
+    One(T),
+    Many(Vec<T>),
+}
 
 #[tracing::instrument(
     name = "Deserialize outcome result into nft model",
     skip(outcome_result)
 )]
-pub fn deserialize_outcome_result_into_token(
+pub fn deserialize_outcome_result_into<T: DeserializeOwned>(
     outcome_result: &ExecutionStatusView,
-) -> anyhow::Result<TokenExt> {
-    let token = match outcome_result {
+) -> anyhow::Result<T> {
+    let ret = match outcome_result {
         ExecutionStatusView::SuccessValue(v) => {
             let bytes = base64::decode(v)?;
-            serde_json::from_slice::<TokenExt>(&bytes)?
+            serde_json::from_slice::<T>(&bytes)?
         }
         _ => return Err(anyhow!("Outcome result is not success value")),
     };
 
-    Ok(token)
+    Ok(ret)
 }
 
 #[tracing::instrument(
@@ -26,30 +34,40 @@ pub fn deserialize_outcome_result_into_token(
     skip(outcome_result, client)
 )]
 pub async fn build_nft_request(
-    event: NftEventKind,
+    event: NftEvent,
     outcome_result: &ExecutionStatusView,
     client: web::Data<reqwest::Client>,
 ) -> anyhow::Result<reqwest::RequestBuilder> {
     let config = get_config().await;
     let base_url = config.rest.base_url();
-    match event {
+    match event.event {
         NftEventKind::NftMint => {
-            let token: NftTokenForRest = deserialize_outcome_result_into_token(outcome_result)
-                .context("Failed to deserialize nft token")?
-                .try_into()
-                .map_err(|_| anyhow::anyhow!("Failed to convert TokenExt to NftTokenForRest"))?;
+            let tokens: OneOrMany<TokenExt> = deserialize_outcome_result_into(outcome_result)
+                .context("Failed to deserialize nft token")?;
+
+            let tokens_ext = match tokens {
+                OneOrMany::One(token) => vec![token],
+                OneOrMany::Many(tokens) => tokens,
+            };
+
+            let tokens_for_rest = tokens_ext
+                .into_iter()
+                .flat_map(|t| NftTokenForRest::try_from(t).ok())
+                .collect::<Vec<_>>();
 
             let request = client
                 .post(format!("{base_url}/nft_tokens"))
                 .header("Content-Type", "application/json")
                 .basic_auth(config.rest.username(), Some(config.rest.password()))
-                .json(&token);
+                .json(&tokens_for_rest);
 
             Ok(request)
         }
         NftEventKind::AssembleNft | NftEventKind::DisassembleNft => {
-            let token: NftTokenForRest = deserialize_outcome_result_into_token(outcome_result)
-                .context("Failed to deserialize nft token")?
+            let token: TokenExt = deserialize_outcome_result_into(outcome_result)
+                .context("Failed to deserialize nft token")?;
+
+            let token_for_rest: NftTokenForRest = token
                 .try_into()
                 .map_err(|_| anyhow::anyhow!("Failed to convert TokenExt to NftTokenForRest"))?;
 
@@ -57,7 +75,7 @@ pub async fn build_nft_request(
                 .patch(format!("{base_url}/nft_tokens"))
                 .basic_auth(config.rest.username(), Some(config.rest.password()))
                 .header("Content-Type", "application/json")
-                .json(&token);
+                .json(&token_for_rest);
 
             Ok(request)
         }
@@ -76,7 +94,7 @@ pub async fn handle_nft_events(
 ) -> anyhow::Result<()> {
     for event in events {
         let outcome_result = &outcome.execution_outcome.outcome.status;
-        let request = build_nft_request(event.event, outcome_result, client.clone()).await;
+        let request = build_nft_request(event, outcome_result, client.clone()).await;
 
         if request.is_err() {
             tracing::error!(
